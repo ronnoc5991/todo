6c2f3ed02c21d5347b132902685737c492cf4c49
Connor Streng <cstreng95@gmail.com>
2025-03-05 19:12:15 +0100

    feat: use esmodules

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
-const express = require('express');
+import express from  'express';
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+	res.send('Got a POST request');
+});
+
+app.put('/', (req, res) => {
+	res.send('Got a PUT request');
 });
 
 app.listen(port, () => {
