e68dcc3841b8a0117c94437a06ec05765d5ab17d
Connor Streng <cstreng95@gmail.com>
2025-07-28 09:27:42 -0400

    feat: ensure DB is created on startup

--- server/src/models/connection.ts
+++ server/src/models/connection.ts
@@ -1,11 +1,14 @@
 import mysql from "mysql2/promise";
 
+const DATABASE_NAME = "todo_app";
+
 // TODO: switch to 'pool'
 const connection = await mysql.createConnection({
   host: process.env.DB_HOST ?? "",
   user: process.env.DB_USER ?? "",
   password: process.env.DB_PASSWORD ?? "",
-  database: process.env.DB_NAME ?? "",
 });
+
+await connection.query(`CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME}`);
 
 export default connection;
